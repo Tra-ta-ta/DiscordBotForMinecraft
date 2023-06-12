2093ee57982bb2a3a53c2fe7cc2ac5023fa49193
Tra-ta-ta <nikitnet.you01@gmail.com>
2023-06-12 11:08:44 +0800

    Недоработанный бот/иду изучать как работает Java Discord API, потом вернусь сюда/ бот должен перепечатывать сообщения из майнкрафта в дс-сервер.

--- src/main/java/me/edinop0jek/discordbotforminecraft/DiscordBotForMinecraft.java
+++ src/main/java/me/edinop0jek/discordbotforminecraft/DiscordBotForMinecraft.java
@@ -1,17 +1,26 @@
 package me.edinop0jek.discordbotforminecraft;
 
+
+import me.edinop0jek.discordbotforminecraft.events.Events;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DiscordBotForMinecraft extends JavaPlugin {
+public final class DiscordBotForMinecraft extends JavaPlugin{
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        JDA jda = JDABuilder.createDefault("OTY0ODk2MjY0MTM3MzUxMTk4.GTDdl1.Y8HfPyy5bcl3ba4WcBr5GiSa5FnWAhLevIAfns")
+                .build();
+
+        Bukkit.getPluginManager().registerEvents(new Events(), this);
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
